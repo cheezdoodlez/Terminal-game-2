bcdd71502f42a00d1146a78120fa614b84f81b39
Zaki <zakiispahany@gmail.com>
2024-10-22 09:24:38 -0400

    New commit

--- app.js
+++ app.js
@@ -42,20 +42,62 @@ const usernames = [
     'jonas',
 ]
 let username 
-do {
+let currentUser 
+let continuePlaying = prompt('(Y/N):') 
+let won = 0
+let retry = false
 
-username = prompt('Cadet what is your name?: ');
-console.log(`Your name is ${username}`)
-// console.log(`${username} you are now in the space station.`)
+function  checkDatabase(userEntry){
+    if(usernames.includes(userEntry.toLowerCase()))  return true
+    return false
+    
+    
+    
+    
+    
+}
 
-} while (username === undefined)
 
-    if(usernames.includes(username)) console.log('This username is already taken')
-       let option1 = prompt(`do you want to continue as ${username}(Y/N)`)
+do { 
+let username 
+if (retry) {
+    username = currentUser
+    } else {username = prompt('Cadet what is your name?: ');
+currentUser = username    }
+if(!username && retry === false){ //i added 1 guard check an example of validation of user input
+    console.log('Cadet name cannot be empty')
+} else if (checkDatabase(username) && retry === false) {
+    console.log(`username already taken..\n\ndo you want to continue as ${currentUser} ?`)
+    let willTry = prompt('(Y/N):')
+    //ternary operator
+    if(willTry.toLowerCase() === 'y') {
+        retry = true
+        } else {
+        retry = false
+        }
+    } else {
+        console.log(`Hey ${username} u are in space station named Prometheus your first mission is to go to planet c67 do you want to continue to c67 ()`)
+        
+    }
+}while (continuePlaying)
+    console.log ("This should execute")
 
-        if(option1 === 'y' || option1 ==='Y'){
-console.log(option1)
-        } 
+
+// do {
+
+// username = prompt('Cadet what is your name?: ');
+// console.log(`Your name is ${username}`)
+// // console.log(`${username} you are now in the space station.`)
+
+// } while (username === undefined)
+
+//     if(usernames.includes(username)) console.log('This username is already taken')
+//        let option1 = prompt(`do you want to continue as ${username}(Y/N)`)
+
+//         if(option1 === 'y' || option1 ==='Y'){
+// console.log(option1)
+
+//         } 
     
 
 
